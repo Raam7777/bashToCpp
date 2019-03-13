580fa0d402f2c823d907f124443c092c097e3b27
Raam7777 <45120776+Raam7777@users.noreply.github.com>
2019-03-13 14:09:12 +0200

    Update BasicCheck.sh

--- BasicCheck.sh
+++ BasicCheck.sh
@@ -1,46 +1,53 @@
 #!/bin/#!/bash
-folderName=$1
 program=$2
-currentfolder=$(pwd)
 res=0
 comp="Fail"
 memory="Fail"
 race="Fail"
 
-cd $folderName
-make
 
-if [[ $? -eq 0 ]]; then
+make > /dev/null 2>&1
+
+if [ $? -eq 0 ]; then
+  echo "Compilation Succeeded"
   comp="Pass"
   valgrind --leak-check=full --error-exitcode=1 ./$program > /dev/null 2>&1
 
-  if [[ $? -ne 1 ]]; then
+  if [ $? -ne 1 ]; then
+    echo "Memory leaks Succeeded"
     memory="Pass"
     valgrind --tool=helgrind --error-exitcode=1 ./$program > /dev/null 2>&1
 
-    if [[ $? -ne 1 ]]; then
+    if [ $? -ne 1]; then
+      echo "Thread race Succeeded"
       race="Pass"
     else
+      echo "Thread race Fail"
       res=$(($res+1))
     fi
 
   else
+    echo "Memory leaks Fail"
     res=$(($res+2))
     valgrind --tool=helgrind --error-exitcode=1 ./$program > /dev/null 2>&1
-    if [[ $? -ne 1 ]]; then
+    if [ $? -ne 1 ]; then
+      echo "Thread race Succeeded"
     else
+      echo "Thread race Fail"
       res=$(($res+1))
 
     fi
   fi
 else
+  echo "Compilation Fail"
     res=$(($res+7))
 
   fi
 
-cd $currentfolder
+
 echo -e "------------------------------------------"
 echo -e "Compilation |  Memory leaks |  Thread race"
 echo -e "$comp        |   $memory        |    $race"
 echo -e "------------------------------------------"
+echo -e "$res"
 exit $res
